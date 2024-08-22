676b7b93ffb9bfda448771039c2610b9b802e02a
DannyFeng <FengzihangCode@outlook.com>
2024-08-22 14:16:26 +0800

    Initial Commit

---
+++ GoQuestionnaire/GoQuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoQuestionnaireApp.swift
+//  GoQuestionnaire
+//
+//  Created by feng on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GoQuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
